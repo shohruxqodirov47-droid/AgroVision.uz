d599fb5d5195a6a096e206990fd174613f256a5b
shohruxqodirov47-droid <shohruxqodirov47@gmail.com>
2026-09-09 22:19:58 +0500

    app.py fayli majburiy yangilandi

--- app.py
+++ app.py
@@ -281,6 +281,7 @@ GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", "")
 GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", "")
 GOOGLE_DISCOVERY_URL = "https://accounts.google.com/.well-known/openid-configuration"
 
+# Majburiy yangilash uchun izoh: login_google marshruti Vercel'ga aniq yuklanishi kerak
 @app.route("/login/google")
 def login_google():
     import requests
